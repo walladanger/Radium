60ecc0512b3ea2b51b223876de8daa6fbbc50f21
walladanger <warwick.andrew.bartrum@gmail.com>
2026-09-19 19:56:53 -0400

    Fix ReplyModelGate download test race

--- web-app/src/containers/__tests__/ReplyModelGate.test.tsx
+++ web-app/src/containers/__tests__/ReplyModelGate.test.tsx
@@ -226,6 +226,8 @@ describe('ReplyModelGate', () => {
     const download = await screen.findByRole('button', {
       name: /replyGate.download/,
     })
+    await waitFor(() => expect(download).toBeEnabled())
+
     fireEvent.click(download)
 
     expect(mocks.pullModelWithMetadata).toHaveBeenCalledWith(
